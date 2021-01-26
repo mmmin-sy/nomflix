74d232567af510ca58c078a5a305e440517360ee
SooyoungMin <salutluvya@gmail.com>
2021-01-26 13:22:36 +0100

    Loading Bar

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-    height: 100vh;
-    width: 100vw;
-    display:flex;
-    justify-content: center;
-    font-size: 50px;
-    margin-top: 20px;
+const LoadingBar = styled.div`
+    margin: 0 auto;
+    margin-top: 50px;
+    border: 8px solid #2d3436;
+    border-top: 8px solid #e74c3c; /* Red */
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    animation: spin 2s linear infinite;
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
 `;
 
-export default () => <Container><span role="img" aria-label="Loading">🌼</span></Container>;
+export default () => <LoadingBar />;
